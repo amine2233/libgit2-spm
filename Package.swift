e84fdf08b15700c64d813e0ec3542f6c420c8f02
Amine Bensalah <amine.bensalah@intech-consulting.fr>
2022-07-06 19:19:51 +0200

    update version to remove v

--- Package.swift
+++ Package.swift
@@ -20,22 +20,22 @@ let package = Package(
 	targets: [
 		.binaryTarget(
 			name: "libgit2",
-			url: "https://github.com/amine2233/libgit2-spm/releases/download/v1.2.1/libgit2.zip",
+			url: "https://github.com/amine2233/libgit2-spm/releases/download/1.2.1/libgit2.zip",
 			checksum: "e6cc5be713dc8e306c7a9464b37f82386edd81b6cd28c0c3e45185939fd90ca0"
 		),
 		.binaryTarget(
 			name: "libssh2",
-			url: "https://github.com/amine2233/libgit2-spm/releases/download/v1.2.1/libssh2.zip",
+			url: "https://github.com/amine2233/libgit2-spm/releases/download/1.2.1/libssh2.zip",
 			checksum: "027093a75d967c78fed719c6d80ce18e98b14903a063fcf6151f8a5591b53a60"
 		),
 		.binaryTarget(
 			name: "libssl",
-			url: "https://github.com/amine2233/libgit2-spm/releases/download/v1.2.1/libssl.zip",
+			url: "https://github.com/amine2233/libgit2-spm/releases/download/1.2.1/libssl.zip",
 			checksum: "b03321b3d183a827c07e2fd778bd63ad1742e50fea651a561d02708108806204"
 		),
 		.binaryTarget(
 			name: "libcrypto",
-			url: "https://github.com/amine2233/libgit2-spm/releases/download/v1.2.1/libcrypto.zip",
+			url: "https://github.com/amine2233/libgit2-spm/releases/download/1.2.1/libcrypto.zip",
 			checksum: "05174ed4d28e17f03a6f527521fc2bd17a7ce977525321780203489e33c7ad55"
 		),
 	]
